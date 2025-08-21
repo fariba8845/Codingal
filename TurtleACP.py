c911f7023c0611876f527455c3768c2ca40c5912
fariba8845 <faribababu2011@gmail.com>
2025-08-21 20:11:55 +0600

    Add files via upload

---
+++ TurtleACP.py
@@ -0,0 +1,16 @@
+import turtle
+
+my_wn= turtle.Screen()
+my_wn.bgcolor("lavender")
+my_wn.title("Kochop")
+my_pen= turtle.Turtle()
+size=0
+
+while True:
+    for i in range(4):
+        my_pen.fd(size+1)
+        my_pen.left(90)
+        size=size-5
+    size=size+1
+
+
